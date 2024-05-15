96860dc4df0d48885a31371f80c2db1f06941d13
AnhVinhTa <166473988+AnhVinhTa@users.noreply.github.com>
2024-05-15 13:51:18 +0700

    Update App.js

--- App.js
+++ App.js
@@ -17,15 +17,6 @@ import DemoAppbar from "./src/DemoAppbar"
 const Stack = createNativeStackNavigator();
 import { PaperProvider } from 'react-native-paper'
 
-import Project1 from './src/Lab1/Project1';
-import Project2 from './src/Lab1/Project2';
-import Project3 from './src/Lab1/Project3';
-import Project4 from './src/Lab1/Project4';
-import Project5 from './src/Lab1/Project5';
-import Project6 from './src/Lab1/Project6';
-import Project7 from './src/Lab1/Project7';
-import Project8 from './src/Lab1/Project8';
-import Calculator from './src/Lab1/Calculator';
 import Contacts from './src/Lab2/components/Contacts';
 import Routes  from './src/Lab2/components/Routes';
 import Store from './src/Lab2/components/Store';
